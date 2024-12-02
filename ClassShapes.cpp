8438fdf2b766dfed49a91c076e6651396d1b4915
ivtipm@outlook.com <ivtipm@outlook.com>
2024-12-02 13:36:56 +0900

    create cpp file module

--- ClassShapes.cpp
+++ ClassShapes.cpp
@@ -1,4 +1,244 @@
 #include "ClassShapes.h"
 
-using namespace std;
+Shapes::Shapes()
+{
+	string s = "Точка";
+	set_name(s);
+}
 
+Shapes::Shapes(string& Name)
+{
+	set_name(Name);
+}
+
+int Shapes::set_name(string& Name)
+{
+	if ((Name != "") && (Name != " "))
+	{
+		name = Name;
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+string Shapes::get_name() const
+{
+	return name;
+}
+
+string Shapes::to_string() const
+{
+	string s = "Name: " + name;
+	return s;
+}
+
+
+
+Rectangle::Rectangle()
+{
+	string s = "Прямоугольник";
+	set_name(s);
+	set_all_sides(1, 1);
+}
+
+Rectangle::Rectangle(string& name, double side1, double side2)
+{
+
+}
+
+void Rectangle::set_side1(double length)
+{
+
+}
+
+void Rectangle::set_side2(double length)
+{
+
+}
+
+void Rectangle::set_all_sides(double length_s1, double length_s2)
+{
+
+}
+
+double Rectangle::get_side1() const
+{
+
+}
+
+double Rectangle::get_side2() const
+{
+
+}
+
+double Rectangle::calc_perim() const
+{
+
+}
+
+double Rectangle::calc_area() const
+{
+
+}
+
+string Rectangle::to_string() const
+{
+
+}
+
+
+
+Triangle_coord::Triangle_coord()
+{
+
+}
+
+Triangle_coord::Triangle_coord(string& name, double x1, double y1, double x2, double y2, double x3, double y3)
+{
+
+}
+
+void Triangle_coord::set_x_v1(double x)
+{
+
+}
+
+void Triangle_coord::set_y_v1(double y)
+{
+
+}
+
+void Triangle_coord::set_x_v2(double x)
+{
+
+}
+
+void Triangle_coord::set_y_v2(double y)
+{
+
+}
+
+void Triangle_coord::set_x_v3(double x)
+{
+
+}
+
+void Triangle_coord::set_y_v3(double y)
+{
+
+}
+
+void Triangle_coord::set_all_vertexs(double x1, double y1, double x2, double y2, double x3, double y3)
+{
+
+}
+
+double Triangle_coord::get_x_v1() const
+{
+
+}
+
+double Triangle_coord::get_y_v1() const
+{
+
+}
+
+double Triangle_coord::get_x_v2() const
+{
+
+}
+
+double Triangle_coord::get_y_v2() const
+{
+
+}
+
+double Triangle_coord::get_x_v3() const
+{
+
+}
+
+double Triangle_coord::get_y_v3() const
+{
+
+}
+
+double Triangle_coord::calc_perim() const
+{
+
+}
+
+double Triangle_coord::calc_area() const
+{
+
+}
+
+string Triangle_coord::to_string() const
+{
+
+}
+
+
+
+Triangle_sides::Triangle_sides()
+{
+
+}
+
+Triangle_sides::Triangle_sides(string& name, double side1, double side2, double side3)
+{
+
+}
+
+void Triangle_sides::set_side1(double length)
+{
+
+}
+
+void Triangle_sides::set_side2(double length)
+{
+
+}
+
+void Triangle_sides::set_side3(double length)
+{
+
+}
+
+void Triangle_sides::set_all_sides(double length_s1, double length_s2, double length_s3)
+{
+
+}
+
+double Triangle_sides::get_side1() const
+{
+
+}
+
+double Triangle_sides::get_side2() const
+{
+
+}
+
+double Triangle_sides::get_side3() const
+{
+
+}
+
+double Triangle_sides::calc_perim() const
+{
+
+}
+
+double Triangle_sides::calc_area() const
+{
+
+}
+
+string Triangle_sides::to_string() const
+{
+
+}
--- ClassShapes.h
+++ ClassShapes.h
@@ -7,18 +7,14 @@ class Shapes
 {
 private:
 	string name;
-	int vertexs;
-	int sides;
 public:
 	Shapes();
-	Shapes(string name, int vertexs, int sides);
-	void set_name(string name);
-	void set_vertexs(int number);
-	void set_sides(int number);
+	Shapes(string& name);
+	int set_name(string& name);
 	string get_name() const;
-	int get_vertexs() const;
-	int get_sides() const;
 	virtual string to_string() const;
+	virtual double calc_perim() const = 0;
+	virtual double calc_area() const = 0;
 };
 
 class Triangle_coord : public Shapes
@@ -38,7 +34,7 @@ private:
 	void set_y_v3(double y);
 public:
 	Triangle_coord();
-	Triangle_coord(string name, int vertexs, int sides, double x1, double y1, double x2, double y2, double x3, double y3);
+	Triangle_coord(string& name, double x1, double y1, double x2, double y2, double x3, double y3);
 	void set_all_vertexs(double x1, double y1, double x2, double y2, double x3, double y3);
 	double get_x_v1() const;
 	double get_y_v1() const;
@@ -46,8 +42,8 @@ public:
 	double get_y_v2() const;
 	double get_x_v3() const;
 	double get_y_v3() const;
-	double calc_perim() const;
-	double calc_area() const;
+	double calc_perim() const override;
+	double calc_area() const override;
 	string to_string() const override;
 };
 
@@ -62,13 +58,13 @@ private:
 	void set_side3(double length);
 public:
 	Triangle_sides();
-	Triangle_sides(string name, int vertexs, int sides, double side1, double side2, double side3);
+	Triangle_sides(string& name, double side1, double side2, double side3);
 	void set_all_sides(double length_s1, double length_s2, double length_s3);
 	double get_side1() const;
 	double get_side2() const;
 	double get_side3() const;
-	double calc_perim() const;
-	double calc_area() const;
+	double calc_perim() const override;
+	double calc_area() const override;
 	string to_string() const override;
 };
 
@@ -77,21 +73,15 @@ class Rectangle : public Shapes
 private:
 	double side1;
 	double side2;
-	double side3;
-	double side4;
 	void set_side1(double length);
 	void set_side2(double length);
-	void set_side3(double length);
-	void set_side4(double length);
 public:
 	Rectangle();
-	Rectangle(string name, int vertexs, int sides, double side1, double side2, double side3, double side4);
-	void set_all_sides(double length_s1, double length_s2, double length_s3, double length_s4);
+	Rectangle(string& name, double side1, double side2);
+	void set_all_sides(double length_s1, double length_s2);
 	double get_side1() const;
 	double get_side2() const;
-	double get_side3() const;
-	double get_side4() const;
-	double calc_perim() const;
-	double calc_area() const;
+	double calc_perim() const override;
+	double calc_area() const override;
 	string to_string() const override;
 };
